387e0bc99a609fb31dce92fe21a20432291da6ae
seanrattigan <sean.rattigan@gmail.com>
2018-05-31 12:11:20 +0000

    Added index page and layout, changed route and method for home

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,15 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello VTOS Class from Flask!  How u doin?'
+app.debug = True
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#if __name__ == "__main__":
+#    app.run()
