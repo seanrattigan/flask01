c7b79349ccfe53d9a526135ddd155ee31f095748
seanrattigan <sean.rattigan@gmail.com>
2018-05-31 14:33:04 +0000

    Added Articles data, route and template

--- flask_app.py
+++ flask_app.py
@@ -15,5 +15,9 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
 #if __name__ == "__main__":
 #    app.run()
